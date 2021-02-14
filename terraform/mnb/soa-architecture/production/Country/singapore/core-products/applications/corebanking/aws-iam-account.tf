2f45932c73b85679e441a826a258afcbe6ecf6d3
Randeep Puthan Purakkal <randeepputhan@gmail.com>
2021-02-14 16:14:13 +0800

    Ingress VPC branch commit

--- terraform/mnb/soa-architecture/production/Country/singapore/core-products/applications/corebanking/aws-iam-account.tf
+++
@@ -1,7 +0,0 @@
-module "iamaccount" {
-  source = "../../../../../../../patterns/aws-iam"
-
-  app_name    = var.app_name
-  environment = var.environment
-
-}
--- terraform/mnb/soa-architecture/production/Country/singapore/retail-channels/applications/mobilebanking/aws-iam-account.tf
+++
@@ -1,7 +0,0 @@
-module "iamaccount" {
-    source = "/Users/ren/Lab/IAC/iac/terraform/aws-iam"
-
-    app_name = var.app_name
-    environment = var.environment
-
-}
